939ab811de0e3d2e69c9f2363bb136047f388c2e
Mark Palmeri <mlp6@duke.edu>
2013-02-27 11:22:25 -0500

    adding function to reslice Nifti1 stack based on specified voxel size

---
+++ reslice_voxel.m
@@ -0,0 +1,22 @@
+function reslice_voxel(nii_file,voxel_size)
+% function reslice_voxel(nii_file,image)
+%
+% Reslice images to specified voxel size using nearest neighbor interpolation
+% 
+% INPUTS:
+%   nii_file (string) - Nifti-1 filename
+%   voxel_size [float float float]
+%       EV9F4: [0.1877 0.1877 0.1877]
+%       ER7B [0.15 0.15 0.15]
+%       MR - case dependent
+%
+% OUTPURS:
+%   new resliced images are saved 
+%
+% EXAMPLE: reslice_voxel('P27_reg_US.nii',[0.1877 0.1877 0.1877);
+
+addpath('/radforce/mlp6/NIFTI_20121012');
+
+resliced_filename = regexprep(nii_file,'.nii','_resliced.nii');
+
+reslice_nii(nii_file,resliced_filename,voxel_size,1,0,2);
